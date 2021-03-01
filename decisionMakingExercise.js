316e724c74828a9e03671b19267ea2a9bf0b8398
Dustin McGuire <dustinmcguire@KCCCs-MacBook-Pro-3.local>
2021-03-01 11:09:46 -0500

    completed decision making exercise except bonus

---
+++ decisionMakingExercise.js
@@ -0,0 +1,29 @@
+// DECISION MAKING EXERCISE
+
+// 1A
+let randomNum = Math.floor(Math.random()*5) + 1;
+console.log(randomNum);
+
+// 1b
+if (randomNum > 3) {
+console.log("Greater than or equal to 4");
+} else if (randomNum > 1) {
+console.log("Equal to 2 or 3");
+} else {
+    console.log("Equal to 1");
+}
+
+// 1c
+if (randomNum != 3) {
+    console.log("Not equal to 3");
+}
+
+// 1d
+if (randomNum !== 3 && randomNum !== 5) {
+    console.log("Not equal to 3 and not equal to 5");
+}
+
+// 1e
+if (randomNum === 2 || randomNum === 4) {
+    console.log("Equal to 2 or equal to 4");
+}
